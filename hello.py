3a2ade7f595dae22233685467bbbb8970d97d35e
Nakha Lim <nakharinz@gmail.com>
2024-04-27 11:22:22 +0700

    add table

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import pandas as pd
+
+#read df from csv iris dataset from UCI url
+url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+df = pd.read_csv(url, header=None)
+df.columns = ['sepal_length','sepal_width','petal_length','petal_width','species']
 
 #Hello world
 st.title('Hello World')
-st.write('Nakharin')
+st.write('This is Iris dataset from UCI')
+st.dataframe(df)
